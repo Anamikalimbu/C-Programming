acfefa64d4b8f1231fcd6f6765d37a2c1dc3f608
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-12 22:01:47 +0545

    C

---
+++ Practices/0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i,a[5];
+    printf("Enter the number:");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+} 
+printf("Array of numbers : \n");
+for(i=0;i<5;i++){
+ printf(" %d \n",a[i]);   
+}
+return 0;
+}
