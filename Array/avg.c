f74686db771b05c25d72e348ddcee7bbe9a8bd15
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-13 21:03:16 +0545

    array

---
+++ Array/avg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+	int a[3];
+	float avg;
+	printf("Enter a marks of 1st subject: ");
+	scanf("%d", &a[0]);
+	printf("Enter a marks of 2nd subject: ");
+	scanf("%d", &a[1]);
+	printf("Enter a marks of 3rd subject: ");
+	scanf("%d", &a[2]);
+	avg=(a[0] + a[1] + a[2])/3.0;
+	printf("The average marks is %2f%%", avg);
+	return 0;
+}
