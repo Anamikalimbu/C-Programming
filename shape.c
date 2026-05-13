18e71d4901548f456c3476c949aedc244ee0aac7
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-13 21:12:06 +0545

    c

---
+++ shape.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    int a,b;
+    for(a=1;a<=10;a++){
+        for (b=1;b<=a;b++)
+        {
+            printf("*",b);
+        }
+        printf("\n");
+    }
+    return 0;
+}
